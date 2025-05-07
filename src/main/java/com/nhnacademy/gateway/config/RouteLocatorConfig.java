501ccde46d8b4e32d839e4b32c92c644ca28cea8
JS01C <cjs010131@gmail.com>
2025-05-07 11:17:18 +0900

    modify(RouteLocatorConfig.java): 주석 변경

--- src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -14,6 +14,7 @@ public class RouteLocatorConfig {
 	 * p->p.path("/account") : /account 경로에 대한 매핑
 	 * .and() 이 이외의 추가 조건을 붙일 때
 	 * .uri("lb://ACCOUNT-SERVICE") ACCOUNT-SERVICE 라는 이름의 API에 로드 밸런싱으로 요청을 보낸다.(유레카에 등록된 이름을 써야 한다.)
+	 * 수정
 	 * */
 
 	@Bean
